c1aee3a6ad0a9d6c1e1a06222d631bd79ae66b0e
Macarena Rodriguez <macarenar697@gmail.com>
2020-07-13 19:22:38 -0300

    Se resolveu divisao com /

--- Java_Script/exercises/4.1/op-aritmeticas.js
+++ Java_Script/exercises/4.1/op-aritmeticas.js
@@ -1,8 +1,8 @@
 //Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Faça programas para:
 
 //ADICAO
-let a = 10;
-let b = 25;
+let a = 25;
+let b = 10;
 
 let suma = a + b;
 console.log(suma);
@@ -13,4 +13,8 @@ console.log(resta);
 
 //Multiplicacao
 let multi = a * b;
+console.log(multi);
+
+//Divisao
+let divi = a / b;
 console.log(multi);
