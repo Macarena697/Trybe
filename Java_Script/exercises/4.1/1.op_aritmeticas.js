7931c1c28f4cd91985716708f8fed0cbdd34b470
Macarena Rodriguez <macarenar697@gmail.com>
2020-07-13 21:55:20 -0300

    Se tracaram os nomes do arquivos para organizar melhor. Se resolveu exercicio tres usando if e else

---
+++ Java_Script/exercises/4.1/1.op_aritmeticas.js
@@ -0,0 +1,33 @@
+//Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+
+////ADICAO
+//let a = 25;
+//let b = 10;
+//
+//let suma = a + b;
+//console.log(suma);
+//
+////SUBTRACAO
+//let resta = a - b;
+//console.log(resta);
+//
+////Multiplicacao
+//let multi = a * b;
+//console.log(multi);
+//
+////Divisao
+//let divi = a / b;
+//console.log(multi);
+//
+////Modulo
+//let modulo = a % b;
+//console.log(modulo);
+
+var a = 10;
+var b = 5;
+
+console.log('Soma: ' + (a + b));
+console.log('Subtração: ' + (a - b));
+console.log('Multiplicação: ' + (a * b));
+console.log('Divisão: ' + (a / b));
+console.log('Módulo: ' + (a % b));
