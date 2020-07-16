49833493d503c35555fb227f82caaa5aa1d159a7
Macarena Rodriguez <macarenar697@gmail.com>
2020-07-16 17:38:21 -0300

    Se refiz com varias soluciones

--- Java_Script/exercises/4.4/01.funcoes-op-aritmeticas.js
+++ Java_Script/exercises/4.4/01.funcoes-op-aritmeticas.js
@@ -1,45 +1,51 @@
-//var a = 10;
-//var b = 5;
-//
-//console.log('Soma: ' + (a + b));
-//console.log('Subtração: ' + (a - b));
-//console.log('Multiplicação: ' + (a * b));
-//console.log('Divisão: ' + (a / b));
-//console.log('Módulo: ' + (a % b));
-let resultado;
 
+//Pegue cada um dos exercícios da primeira parte das //nossas aulas de JavaScript e faça com que todos eles //sejam funções de um mesmo arquivo. As variáveis que você //define no começo de cada arquivo devem ser removidas e //transformadas em parâmetros das funções.
+
+// Formas diferentes de fazer
+
+// 1-------------------------
 function suma (a, b){
-  resultado = a + b;
-  return resultado;
+  return a + b;
 }
+let resultado = suma (10, 15);
+console.log (resultado);
 
+// 2-------------------------
 function subtr (a, b){
-  resultado = a - b;
-  return resultado;
+  return a - b;
 }
+console.log (subtr(50, 30));
 
+// 3-------------------------
 function multi (a, b){
   resultado = a * b;
   return resultado;
 }
+multi (10, 50);
+console.log (resultado);
 
+//------------------------
 function divi (a, b){
   resultado = a / b;
   return resultado;
 }
+divi (50, 2);
+console.log (resultado);
 
+//--------------------------
 function modulo (a, b){
   resultado = a % b;
   return resultado;
 }
-
-suma (10, 15);
-console.log (resultado);
-subtr (50, 30);
-console.log (resultado);
-multi (10, 50);
-console.log (resultado);
-divi (50, 2);
-console.log (resultado);
 modulo (2, 6);
 console.log (resultado);
+
+
+
+
+
+
+
+
+
+
