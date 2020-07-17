43d883c06ef6d05d76aa2671e16e4c680d6d999b
Macarena Rodriguez <macarenar697@gmail.com>
2020-07-16 21:12:55 -0300

    se imprimio nome mais largo usando for in e uma condicao dentro de if para comparar

--- Java_Script/exercises/4.4/funcoes/04.maior_cantidad_caracter.js
+++ Java_Script/exercises/4.4/funcoes/04.maior_cantidad_caracter.js
@@ -5,3 +5,16 @@
 //Valor esperado no retorno da função: Fernanda.
 
 let nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+function maiorNome (nomes){
+  let indiceMaior = 0;
+  let nomeMaior;
+  for (indice in nomes){
+    if (nomes[indice].length > indiceMaior){
+      indiceMaior = nomes[indice].length;
+      nomeMaior = nomes[indice];
+    }
+  }
+  return nomeMaior;
+}
+console.log(maiorNome(nomes));
