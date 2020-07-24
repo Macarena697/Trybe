4c6bff4494cc7c29ad0126c3b8820ac8445237e9
Macarena Rodriguez <macarenar697@gmail.com>
2020-07-24 15:19:40 -0300

    Exercicios do dia 3 do modulo 4 de Java Script - Ainda falta terminar

---
+++ 02.triangulo_asteriscos.js
@@ -0,0 +1,11 @@
+//Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+let cantidadAstericos = 5;
+let asteriscosLinea = [];
+
+if (cantidadAstericos > 1){
+  for (let i = 0; i < cantidadAstericos; i += 1){
+    asteriscosLinea += "*"; 
+    console.log(asteriscosLinea);
+  }
+}
