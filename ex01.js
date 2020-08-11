db8b45e89b877688b480fae68d82e88444c500b2
Macarena Rodriguez <macarenar697@gmail.com>
2020-08-11 20:24:44 -0300

    Exercicios do dia

--- ex01.js
+++ ex01.js
@@ -57,3 +57,13 @@ function counterPupils() {
   return allLessons.lesson1.numeroEstudantes + allLessons.lesson2.numeroEstudantes + allLessons.lesson3.numeroEstudantes
 }
 
+//const getNumberOfStudents = (obj) => {
+//  let total = 0;
+//  const array = Object.keys(obj);
+//  for (i in array) {
+//    total += obj[array[i]].numeroAlunos;
+//  }
+//  return total;
+//};
+//console.log(getNumberOfStudents(allLessons));
+
