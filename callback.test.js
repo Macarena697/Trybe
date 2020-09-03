366c2ca51d6de617b1862461bbbac26b18518367
Macarena Rodriguez <macarenar697@gmail.com>
2020-09-03 18:37:44 -0300

    cONTEUDO

---
+++ callback.test.js
@@ -0,0 +1,16 @@
+const SumNumbers = require('./callback')
+
+test('Testando SumNumbers, soma 5 mais 10', done => {
+  SumNumbers(5, 10, (result) => {
+    expect(result).toBe(15);
+    done();
+  });
+})
+
+test("Não deveria passar!", done => {
+  setTimeout(() => {
+    expect(10).toBe(5);
+    console.log('Deveria falhar!')
+    done();
+  }, 500);
+});
