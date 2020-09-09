0c3776bcbcd9b1ad436f0e8df0b0648ff2179fba
Macarena Rodriguez <macarenar697@gmail.com>
2020-09-09 19:03:40 -0300

    Exercicio do conteudo-etorne um <h1> com o seu nome um paragráfo, <p>, com uma breve descrição sobre você.

---
+++ testando-meu-computador/src/Component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Macarena</h1>
+        <p>
+          dknfvlkdnlkfnlkdnfl
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Component;
