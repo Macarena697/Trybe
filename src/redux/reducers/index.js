67a256f0b9ffee3b2ec3c7dd6b57cc45126d8679
Macarena Rodriguez <macarenar697@gmail.com>
2020-12-10 19:30:56 -0300

    FORM React-Redux

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,6 @@
 const INIATIAL_STATE = {
   text:"",
-  email:'',
+  check:'',
   idade:0,
 }
 
@@ -9,7 +9,7 @@ function reducer(state = INIATIAL_STATE, action) {
     case 'ADD_TEXT':
       return { ...state, text: action.value }
     case 'ADD_CHECK':
-      return { ...state, email: action.value }
+      return { ...state, check: action.value }
     case 'ADD_AGE':
       return { ...state, idade: action.value }
     default:
